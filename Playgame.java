c455a5bfaf55dd8d7aa4be128ba53c655c8e715a
gabeneuman <119382880+gabeneuman@users.noreply.github.com>
2022-11-29 18:19:53 -0500

    main and evenorodd

--- Playgame.java
+++ Playgame.java
@@ -6,24 +6,28 @@ public class Playgame {
         int UP = 0; //user points
         int CP = 0; //computer points
         int choice = 0;
-        String inp;
+        String input;
         Scanner scnr = new Scanner(System.in);
         boolean cont = true;
+        int winner = 0; //used as return value from games. 1 if user wins, 0 if computer wins
 
         System.out.println("\n\nWelcome to the Game of Games! Within this game you can play 5 fun games against the computer:\nFind the Thimble, Coin Flip, Guess the Number, Even and Odd, or Find the Red Thread. \nYou will select a game to play, play through it against the computer, \nand then whoever wins that game will get a point on an overall scoreboard for this Game of Games. \nAfter each game you play, this scoreboard will be displayed. \nYou will play these games repeatedly until you decide to quit. \nOnce you quit, the final tally will be displayed on the scoreboards to show the overall number of games won and lost by each player, \nand finally which player won the most, that is, which player won the Game of Games. \n\nLet's begin!\n");
+        
+        //runs until player wished to quit
         while (cont){
             System.out.println("Games:\n1. Find the Thimble\n2. Flip the coin\n3. Guess the Number\n4. Even and Odd\n5. Find the Red Thread\n");
             System.out.println("Pick a game to play! To choose a game, enter the corresponding integer with that game.\nChoice: ");
-            inp = scnr.nextLine();
             while (1==1){
-                if (inp.charAt(0)<49 || inp.charAt(0)> 53){
-                    System.out.println("Input Invalid. You must enter an integer that corresponds to a game. Try again!");
+                input = scnr.nextLine();
+                if (input.charAt(0)<49 || input.charAt(0)> 53){
+                    System.out.println("inputut Invalid. You must enter an integer that corresponds to a game. Try again!");
                     continue;
                 }
                 break;
             }
 
-            choice = (int)inp.charAt(0);
+            choice = (int)input.charAt(0)- 48;
+
             switch(choice){
                 case 1:
                     //find the thimble
@@ -40,7 +44,15 @@ public class Playgame {
                     //guess the number
                     break;
                 case 4:
-                    //even and odd
+                    evenOrOdd game = new evenOrOdd();
+                    winner = game.game();
+                    if (winner == 1){
+                        UP++;
+                    }
+                    else{
+                        CP++;
+                    }
+                    displayScoreboard(UP, CP);
                     break;
                 case 5:
                     //find the red thread
@@ -51,16 +63,16 @@ public class Playgame {
             System.out.println("Do you want to continue to play the Game of Games?\nEnter 'Y' for yes and 'N' if you want to quit");
 
             while (1==1){
-                inp = scnr.nextLine();
-                if (inp.charAt(0) == 'Y' || inp.charAt(0) == 'y'){
+                input = scnr.nextLine();
+                if (input.charAt(0) == 'Y' || input.charAt(0) == 'y'){
                     break;
                 }
-                else if(inp.charAt(0) == 'N' || inp.charAt(0) == 'n' ){
+                else if(input.charAt(0) == 'N' || input.charAt(0) == 'n' ){
                     cont = false;
                     break;
                 }
                 else{
-                    System.out.println("Invalid input. You must enter either 'Y' or 'N'\n");
+                    System.out.println("Invalid inputut. You must enter either 'Y' or 'N'\n");
 
                 }
             }
--- evenOrOdd.java
+++ evenOrOdd.java
@@ -1,23 +1,107 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class evenOrOdd {
-    public static int evenOrOdd(){
-        int bestOutOf;
-        String userChoice;
-        String computerChoice;
-        int userThrow;
-        int computerThrow;
-        int userPoints;
-        int computerPoints;
-        String input;
-        Scanner scnr = new Scanner(System.in);
+    int bestOutOf;
+    
+    //these two represent remainder when divided by 2. Ex if userChoice ==1, then that would be odd becasue oddNum%2 = 1
+    int userChoice;
+    int computerChoice;
 
+    int userThrow;
+    int computerThrow;
+    int userPoints;
+    int computerPoints;
+    String input;
+
+    evenOrOdd(){
+        userPoints = 0;
+        computerPoints = 0;
+    }
+
+    public int game(){
+        Scanner scnr = new Scanner(System.in);
+        Random rand = new Random();
         System.out.println("Even and Odd Game:\nThe player will choose even or odd. You will remain even or odd for the whole game and cannot switch from throw to throw. Valid throws are integer values from 1 to 5. The user will also indicate the “best out of number”. This will be how many games are played to win. For example, If you choose 7, the first to win 4 throws wins the game. The 'best out of number' has to be odd.\n");
+        
+        //choosing how many games to play
         System.out.println("Pick a best out of number that will be played too. The number has to be an integer.\nBest out of number: \n");
-        input = scnr.next();
         while(1==1){
-            if (input.charAt(0))
+            input = scnr.nextLine();
+            if (input.charAt(0) == (int) input.charAt(0) && ((int)input.charAt(0)-48) % 2 == 1 && ((int)input.charAt(0)-48) > 0){
+                bestOutOf = (int) input.charAt(0) - 48;
+                break;
+            }
+            else{
+                System.out.println("Invalid choice. Please enter a positive odd number\nBest out of number:\n");
+            } 
         }
+
+        //choosing even or odd
+        System.out.println("Pick your choice of even or odd. Your choice will be the same for the whole game and you won’t be able to switch from throw to throw.\nEnter 'o' for odd or 'e' for even: ");
+        while(1==1){
+            input = scnr.nextLine();
+            if(input.charAt(0) == 'e' || input.charAt(0) == 'E'){
+                userChoice = 0;
+                computerChoice = 1;
+                System.out.println("Your choice is even\n");
+                break;
+
+            }
+            else if(input.charAt(0) == 'o' || input.charAt(0) == 'O'){
+                userChoice = 1;
+                computerChoice = 0;
+                System.out.println("Your choice is odd\n");
+                break;
+            }
+            else{
+                System.out.println("Invalid choice. Plese input 'e' for even or 'o' for odd: ");
+            }
+        }
+
+        while(userPoints < (bestOutOf/2+1) && computerPoints < (bestOutOf/2+1)){
+            System.out.println("\nPick an integer between 1 and 5. This is your throw for the round: ");
+            while (1==1){
+                input = scnr.nextLine();
+                if (input.charAt(0)<49 || input.charAt(0)> 53){
+                    System.out.println("Invalid input. Please pick an integer between 1 and 5");
+                    continue;
+                }
+                break;
+            }
+            userThrow = (int) input.charAt(0) - 48;
+            computerThrow = 1 + rand.nextInt(5);
+
+
+            System.out.println("Your choice for this round is: "+userThrow+"\nThe computers choice for this round is: "+computerThrow);
+            if((userThrow + computerThrow)%2 == 0){
+                System.out.println("The throws combined is "+(userThrow+computerThrow)+" which is an even number.\n");
+            }
+            else{
+                System.out.println("The throws combined is "+(userThrow+computerThrow)+" which is an odd number.\n");
+            }
+
+            if((userThrow + computerThrow)%2 == userChoice){
+                System.out.println("You won this round!");
+                userPoints++;
+            }
+            else{
+                System.out.println("The computer won this round.");
+                computerPoints++;
+            }
+            System.out.println("User points: "+userPoints+"\nComp points: "+computerPoints);
+        }
+
+        if(userPoints == (bestOutOf/2+1)){
+            System.out.println("Congratulations you won!");
+            return 1;
+        }
+        else{
+            System.out.println("Game Over. You lost against the computer. Better luck next time.");
+            return 0;
+        }
+
+
 
     }
     
