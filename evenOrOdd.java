90342b4ff65aa1fc4276dc0d0caffe8aa1979a51
gabeneuman <119382880+gabeneuman@users.noreply.github.com>
2022-11-29 16:42:28 -0500

    Create evenOrOdd.java

---
+++ evenOrOdd.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class evenOrOdd {
+    public static int evenOrOdd(){
+        int bestOutOf;
+        String userChoice;
+        String computerChoice;
+        int userThrow;
+        int computerThrow;
+        int userPoints;
+        int computerPoints;
+        String input;
+        Scanner scnr = new Scanner(System.in);
+
+        System.out.println("Even and Odd Game:\nThe player will choose even or odd. You will remain even or odd for the whole game and cannot switch from throw to throw. Valid throws are integer values from 1 to 5. The user will also indicate the “best out of number”. This will be how many games are played to win. For example, If you choose 7, the first to win 4 throws wins the game. The 'best out of number' has to be odd.\n");
+        System.out.println("Pick a best out of number that will be played too. The number has to be an integer.\nBest out of number: \n");
+        input = scnr.next();
+        while(1==1){
+            if (input.charAt(0))
+        }
+
+    }
+    
+}
