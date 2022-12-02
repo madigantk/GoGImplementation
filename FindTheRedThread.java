155f28fd4f368a3ec8b915c7c0dd3d133132c732
maddietk7 <mtershne@trincoll.edu>
2022-12-02 17:28:04 -0500

    Update to FIndTheRedThread

--- FindTheRedThread.java
+++ FindTheRedThread.java
@@ -9,6 +9,9 @@ public class FindTheRedThread{
   int ComputerScore;
   String[] PossibleColors = new String[]{"blue", "green", "yellow"};
   int WhosTurn;
+  int CompCount = 0;
+  int PlayerCount = 0;
+
 
   FindTheRedThread(){
     PlayerScore = 0;
@@ -20,15 +23,16 @@ public class FindTheRedThread{
 
     System.out.println("Available spools: ");
     for(int i = 0; i < Spools.length; i++){
-      if(Spools[i] != "Picked"){
-        System.out.printf("Spool S%d   ", i+1);
+      if(Spools[i] != "picked"){
+        System.out.printf("Spool %d   ", i+1);
       }
     }
+    System.out.println();
   }
 
 
 
-  public int game(){
+  public int run(){
     Scanner reader = new Scanner(System.in);
     Random rand = new Random();
 
@@ -58,62 +62,65 @@ public class FindTheRedThread{
       System.out.println("Computer goes first");
     }
     else{
-      System.out.println("You go first")
+      System.out.println("You go first");
     }
 
     while(1==1){
-      if(WhosTurn = 0){
-        int CompCount = 0;
+      if(WhosTurn == 0){
+        int CompPick = -1;
         System.out.println("It is the computer's turn");
         PrintSpools();
-        int CompPick = -1;                        //Making sure the computer chooses a spool that is available
-        while(CompPick = -1){
+        while(CompPick == -1){                  ////Making sure the computer chooses a spool that is available
           CompPick = rand.nextInt(20);
-          if(Spools[CompPick+1] == "picked"){
+          if(Spools[CompPick-1] == "picked"){
             CompPick = -1;
           }
         }
 
         System.out.printf("The computer picked spool %d%n", CompPick);
-        System.out.printf("The color of spool %d is %s%n", CompPick, Spools[CompPick]);
-        Spools[CompPick] = "picked";
+        System.out.printf("The color of spool %d is %s%n%n%n", CompPick, Spools[CompPick-1]);
         CompCount++;
-        if(Spools[CompPick+1] == "red"){
+        if(Spools[CompPick-1] == "red"){
           System.out.println("Game Over. You lost against the computer. Better luck next time.");
           return 0;
         }
+        Spools[CompPick-1] = "picked";
         if(CompCount == SpoolsPerTurn){
           WhosTurn = 1;
+          PlayerCount = 0;
         }
       }
 
       else{                                   //Player's Turn
-        int PlayerCount = 0;
-        int PlayerPick = -1
+        int PlayerPick = -1;
         System.out.println("It is your turn");
         PrintSpools();
+        System.out.println();
         while(PlayerPick == -1){
           try{
             System.out.println("Pick a spool from the available options. Enter the integer of the spool you want to pick (1 for S1, 2 for S2, etc.)");
             PlayerPick = reader.nextInt();
-            if(Spools[PlayerPick+1] == "picked"){
+            if(Spools[PlayerPick-1] == "picked" || PlayerPick < 1 || PlayerPick > 20){
               System.out.println("Error. You must pick a spool that is available (look at the display list of all available spools).");
               PlayerPick = -1;
             }
           }
           catch(Exception e){
             System.out.println("Error. You must enter an integer. Try again.");
+            PlayerPick = -1;
           }
         }
         PlayerCount++;
-        System.out.printf("You picked spool S%d%n", PlayerPick);
-        System.out.printf("The color of spool S%d is %s%n", PlayerPick, Spools[PlayerPick]);
-        if(Spools[PlayerPick+1] == "red"){
+        System.out.printf("You picked spool %d%n", PlayerPick);
+        System.out.printf("The color of spool %d is %s%n%n%n", PlayerPick, Spools[PlayerPick-1]);
+        if(Spools[PlayerPick-1] == "red"){
           System.out.println("Congratulations, you won!");
           return 1;
         }
+        Spools[PlayerPick-1] = "picked";
         if(PlayerCount == SpoolsPerTurn){
           WhosTurn = 0;
+          CompCount = 0;
         }
 
 
