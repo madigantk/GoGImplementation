c1255b927eddfa5d0aa6f542fcc9113475adc199
sokp13 <119386197+sokp13@users.noreply.github.com>
2022-12-04 16:44:10 -0500

    Update coinFlip.java

--- coinFlip.java
+++ coinFlip.java
@@ -13,6 +13,7 @@ public class coinFlip extends getInput{
 		
 		boolean flag = false; 
 
+	// THE METHOD THAT RUNS THE ACTUAL GAME //
 	public int run(){
 		
 		UP = 0;
@@ -24,11 +25,13 @@ public class coinFlip extends getInput{
 						+ "If the player guesses correctly they get a point and if it’s incorrect, the computer earns a point.\n\n"
 						+ "Whoever has the most points after the agreed total games wins a point that will be added to their overall score board of Games of games.\n\n");
 
+		// ENTERING THE BEST-OF NUMBER TO PLAY OUT OF //
 		System.out.println("\nEnter the 'best out of number' you want to play –– this must be an odd integer. The player with the most points will be declared as the winner after playing this total number of games.\n");
 		min = bestOfMin(getBestOf());
 
 		System.out.println("\nYou will be the guesser throughout the game.");
-				
+		
+		// THE INPUT OF GUESSING HEADS/TAILS //
 		while(!flag){
 			
 			System.out.println("\nEnter your guess. Enter ‘heads’ to guess the coin will land on heads, or ‘tails’ to guess the coin will land on tails.\n");
@@ -37,16 +40,19 @@ public class coinFlip extends getInput{
 			System.out.println("You have guessed " + guess + ".\n");
 			System.out.println("Flipping the coin....\n");
 			
+			// SIMULATE/RETURN THE COIN FLIP //
 			flip = flipCoin();
 
 			System.out.println("The coin landed on " + flip + ".\n");
 
+			// EVALUATING THE GUESS AND THE COIN FLIP RESULT //
 			evaluate(flip, guess);
 			if(UP == min || CP == min){
 				flag = true;
 			}
 		}
-
+		
+		// CONCLUSION GETS ANALYZED AND WIN/LOSS GETS RETURNED //
 		conclusion();
 		return gog_score;
 		}
@@ -54,7 +60,8 @@ public class coinFlip extends getInput{
 	public int bestOfMin(int a){
 		int out = ((a/2)+1);
 		return out;}
-
+	
+	// The evaluation of who wins //
 	public void evaluate(String f, String g){
 		if(f.equalsIgnoreCase(g)){
 			System.out.println("Correct guess, the coin landed on " + f + "! You've earned a point.");
@@ -65,7 +72,8 @@ public class coinFlip extends getInput{
 			CP++;
 			//System.out.println(CP + " computer wins.");
 		}}
-
+	
+	// Setting the return/Printing of who won the series //
 	public void conclusion(){
 		if(UP > CP){
 			System.out.println("\nCongratulations, you won!\n");
@@ -75,6 +83,7 @@ public class coinFlip extends getInput{
 			gog_score = 0;
 		}}
 
+	// Simulation of the actual flipping of the coin //
 	public String flipCoin(){
 		Random rand = new Random();
 
